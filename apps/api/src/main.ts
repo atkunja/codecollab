7834e35d9c16eb6032851247104a53cc51015e91
Ayush Kunjadia <114869544+atkunja@users.noreply.github.com>
2025-07-11 02:22:04 -0400

    fix: backend build and start scripts

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   // ENABLE CORS SO FRONTEND CAN TALK TO BACKEND
   app.enableCors({
     origin: [
-      'https://codecollab-uuoyp6clb-atkunjas-projects.vercel.app', // Vercel app URL
+      'https://codecollab-bjimey94s-atkunjas-projects.vercel.app/', // Vercel app URL
       'http://localhost:3002', // local Next.js dev (optional)
     ],
     credentials: true,
