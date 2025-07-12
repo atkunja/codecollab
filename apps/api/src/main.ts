6fa7e73b1394a30052a3c8b38b0da3d271d5fb63
Ayush Kunjadia <114869544+atkunja@users.noreply.github.com>
2025-07-12 00:59:13 -0400

    fix: backend build and start scripts

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,21 +1,23 @@
+// main.ts
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
-import { AppModule }  from './app.module';
-import * as cors      from 'cors';
+import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(
     cors({
-      origin: '*', // Allow all origins (for debug). Restrict later!
+      origin: '*', // for now, to debug CORS, allow all
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
       credentials: true,
     }),
   );
 
-  const port = process.env.PORT ? +process.env.PORT : 3001;
-  await app.listen(port, '0.0.0.0');
-  console.log(`🚀 API listening on http://0.0.0.0:${port}`);
+  // Railway likes port 3000!
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(` API listening on http://0.0.0.0:${port}`);
 }
 bootstrap();
