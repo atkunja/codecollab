9fec141073e5de7c8ce23ee31520ee459bd85cb3
Ayush Kunjadia <114869544+atkunja@users.noreply.github.com>
2025-07-12 01:44:24 -0400

    fix: backend build and start scripts

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,26 +1,11 @@
+// main.ts
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  // Add a logger to see all incoming requests
-  app.use((req, res, next) => {
-    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
-    next();
-  });
-
-  app.use(
-    cors({
-      origin: '*',
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-      credentials: true,
-    }),
-  );
-
-  const port = process.env.PORT || 8080;
+  const port = process.env.PORT || 3001; // fallback 3001
   await app.listen(port, '0.0.0.0');
   console.log(`🚀 API listening on http://0.0.0.0:${port}`);
 }
