ccef584ecd5c6dd82b4b763de1929692cc9d4bb7
Ayush Kunjadia <atkunjadia@gmail.com>
2025-10-04 14:47:51 -0400

    feat: add supabase packages

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -2,5 +2,5 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ["/((?!api/auth|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/room/:path*", "/create-room"],
 };
